d137a4376c46546dee1c2501c35f99016610e2a9
Gal Zohar <galzohar10@gmail.com>
2018-03-26 00:56:18 +0300

    three first screens more or less

---
+++ app/src/main/java/com/kalay/shift/shift/InterestsActivity.java
@@ -0,0 +1,17 @@
+package com.kalay.shift.shift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by gal zohar on 3/25/2018.
+ */
+
+public class InterestsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interests);
+    }
+
+}
