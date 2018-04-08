9e8ca61daef6d5f8af554cb9e5691590caf8adc7
Ori Mandelawi <omandelawi@gmail.com>
2018-04-08 18:13:21 +0300

    start the prefrences changes

---
+++ app/src/main/java/com/kalay/shift/shift/prefrencesChangesActivity.java
@@ -0,0 +1,29 @@
+package com.kalay.shift.shift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import java.lang.reflect.Array;
+
+public class prefrencesChangesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prefrences_changes);
+
+        //read input array
+        String names[] = {"golf", "tennis"};
+
+        for (int i=0; i< names.length ;i++) {
+            //create the UI check box
+            final LinearLayout ll = findViewById(R.id.linearLayoutId);
+            CheckBox cb = new CheckBox(getApplicationContext());
+            cb.setText(names[i]);
+            ll.addView(cb);
+
+        }
+    }
+}
