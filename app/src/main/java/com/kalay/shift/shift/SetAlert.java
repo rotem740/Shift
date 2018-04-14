8f678b3166d74b9d042abb09c5bf61be27605fd2
Rom Dolinger <romdolinger@gmail.com>
2018-04-14 18:15:10 +0300

    Add personal alert page
    Add personal alert page

---
+++ app/src/main/java/com/kalay/shift/shift/SetAlert.java
@@ -0,0 +1,15 @@
+package com.kalay.shift.shift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetAlert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_alert);
+    }
+
+
+}
