a2bb329a2a3b39e06d117c6d3dd0a379642e7877
galzr <galzohar10@gmail.com>
2018-04-14 22:23:59 +0300

    notifications using service

--- app/src/main/java/com/kalay/shift/shift/MainActivity.java
+++ app/src/main/java/com/kalay/shift/shift/MainActivity.java
@@ -1,6 +1,13 @@
 package com.kalay.shift.shift;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +15,79 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    ////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        doBindService();
+    }
+
+    static LocalService mBoundService;
+    private boolean mShouldUnbind;
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  Because we have bound to a explicit
+            // service that we know is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            mBoundService = ((LocalService.LocalBinder)service).getService();
+
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            // Because it is running in our same process, we should never
+            // see this happen.
+            mBoundService = null;
+
+        }
+    };
+
+    void doBindService() {
+        // Attempts to establish a connection with the service.  We use an
+        // explicit class name because we want a specific service
+        // implementation that we know will be running in our own process
+        // (and thus won't be supporting component replacement by other
+        // applications).
+        if (bindService(new Intent(this, LocalService.class),
+                mConnection, Context.BIND_AUTO_CREATE)) {
+            mShouldUnbind = true;
+        } else {
+            Log.e("MY_APP_TAG", "Error: The requested service doesn't " +
+                    "exist, or this client isn't allowed access to it.");
+        }
+    }
+
+    void doUnbindService() {
+        if (mShouldUnbind) {
+            // Release information about the service's state.
+            unbindService(mConnection);
+            mShouldUnbind = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        doUnbindService();
+    }
+
+    public void displayNotification(View v)
+    {
+        Intent intent = new Intent();
+        PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
+        Notification noti = new Notification.Builder(MainActivity.this)
+                .setContentTitle("מתי לאחרונה יצאת להליכה בחוץ?")
+                .setContentText("נראה לנו שעכשיו הוא הזמן המתאים!")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pIntent).getNotification();
+        noti.flags = Notification.FLAG_AUTO_CANCEL;
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(0,noti);
     }
 
     public void onStart(View v) {
@@ -20,5 +95,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
         startActivity(intent);
     }
+
 
 }
