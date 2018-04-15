1ce9c7b74c6dfb47f91211a4d3592d43b2749a1a
Ori Mandelawi <omandelawi@gmail.com>
2018-04-15 17:14:35 +0300

    started the prefrences changes page

--- app/src/main/java/com/kalay/shift/shift/prefrencesChangesActivity.java
+++ app/src/main/java/com/kalay/shift/shift/prefrencesChangesActivity.java
@@ -15,9 +15,10 @@ public class prefrencesChangesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_prefrences_changes);
 
         //read input array
-        String names[] = {"golf", "tennis"};
-
-        for (int i=0; i< names.length ;i++) {
+        String names[] = {"גולף", "טניס", "שחייה"};
+        System.out.println("ספורט:");
+        for (int i=0; i< names.length ;i++)
+        {
             //create the UI check box
             final LinearLayout ll = findViewById(R.id.linearLayoutId);
             CheckBox cb = new CheckBox(getApplicationContext());
