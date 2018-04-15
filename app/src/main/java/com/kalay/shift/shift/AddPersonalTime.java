e9b58132545927be98272c06fcf9176d42e95571
Rom Dolinger <romdolinger@gmail.com>
2018-04-15 15:05:11 +0300

    SharePreferences
    SharePreferences + test

--- app/src/main/java/com/kalay/shift/shift/AddPersonalTime.java
+++ app/src/main/java/com/kalay/shift/shift/AddPersonalTime.java
@@ -1,15 +1,19 @@
 package com.kalay.shift.shift;
 
+
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-
+import com.kalay.shift.shift.MainActivity;
 import com.mcsoft.timerangepickerdialog.RangeTimePickerDialog;
+import static com.kalay.shift.shift.SharePrefrencesTest.MyPREFERENCES;
 
 
 /**
@@ -17,23 +21,25 @@ import com.mcsoft.timerangepickerdialog.RangeTimePickerDialog;
  */
 
 public class AddPersonalTime extends AppCompatActivity implements RangeTimePickerDialog.ISelectedTime {
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_time);
-
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        Toast.makeText(AddPersonalTime.this,"ed1: " + editor.toString() ,Toast.LENGTH_LONG).show();
         //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.spinner);
         //create a list of items for the spinner.
-        String[] items = new String[]{"first alert", "2", "3"};
+        Integer[] items = new Integer[]{1,2,3};
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         //There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
 
-        // Create an instance of the dialog fragment and show it
         // Create an instance of the dialog fragment and show it
         RangeTimePickerDialog dialog = new RangeTimePickerDialog();
         dialog.newInstance();
@@ -44,11 +50,7 @@ public class AddPersonalTime extends AppCompatActivity implements RangeTimePicke
         FragmentManager fragmentManager = getFragmentManager();
         dialog.show(fragmentManager, "");
 
-
     }
-
-
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
@@ -61,6 +63,8 @@ public class AddPersonalTime extends AppCompatActivity implements RangeTimePicke
                     int hourEnd = data.getExtras().getInt(RangeTimePickerDialog.HOUR_END);
                     int minuteStart = data.getExtras().getInt(RangeTimePickerDialog.MINUTE_START);
                     int minuteEnd = data.getExtras().getInt(RangeTimePickerDialog.MINUTE_END);
+
+
 
                 }
         }
