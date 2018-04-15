5acea9a3ae12efd3f499c81c49df0acb42a5389d
barg2002 <37020360+barg2002@users.noreply.github.com>
2018-04-15 18:23:16 +0300

    added constructor

--- app/src/main/java/com/kalay/shift/core/User.java
+++ app/src/main/java/com/kalay/shift/core/User.java
@@ -1,6 +1,11 @@
 package com.kalay.shift.core;
 
 public class User {
-    private String userName;
+    private String userName; 
     private boolean gender; //true = female, false = male
+
+    public User(String userName, boolean gender){
+        this.userName = userName;
+        this.gender = gender;
+    }
 }
