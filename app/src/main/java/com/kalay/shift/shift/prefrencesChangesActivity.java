44e568b2e3aba7873cd85b68173ddd34bd8a2459
Ron17Amit <37795239+Ron17Amit@users.noreply.github.com>
2018-05-13 13:54:22 +0300

    Add classes in preferencesChangesActivity
    Add

--- app/src/main/java/com/kalay/shift/shift/prefrencesChangesActivity.java
+++ app/src/main/java/com/kalay/shift/shift/prefrencesChangesActivity.java
@@ -1,13 +1,18 @@
 package com.kalay.shift.shift;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class prefrencesChangesActivity extends AppCompatActivity {
+    ArrayList<CheckBox> check1 = new ArrayList<CheckBox>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +21,23 @@ public class prefrencesChangesActivity extends AppCompatActivity {
 
         //read input array
         String names[] = {"גולף", "טניס", "שחייה"};
-        System.out.println("ספורט:");
-        for (int i=0; i< names.length ;i++)
-        {
+        for (int i = 0; i < names.length; i++) {
             //create the UI check box
             final LinearLayout ll = findViewById(R.id.linearLayoutId);
             CheckBox cb = new CheckBox(getApplicationContext());
             cb.setText(names[i]);
             ll.addView(cb);
+            check1.add(cb);
 
         }
+    }
+    public void saveChanges(View v) {
+        //todo save the updated alert
+        ArrayList<String> check_info = new ArrayList<String>();
+        for (CheckBox c1: check1) {
+            if (c1.isChecked())
+                check_info.add(c1.getContext().toString());
+        }
+        FieldsOfInterest f1 = new FieldsOfInterest(this, check_info);
     }
 }
