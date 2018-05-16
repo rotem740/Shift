c4ea5c1a70ec576b560b3af46f987c3c326f5fc3
Rom Dolinger <romdolinger@gmail.com>
2018-05-16 09:17:29 +0300

    Button

--- app/src/main/java/com/kalay/shift/shift/AddPersonalTime.java
+++ app/src/main/java/com/kalay/shift/shift/AddPersonalTime.java
@@ -1,6 +1,7 @@
 package com.kalay.shift.shift;
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
@@ -22,8 +23,8 @@ import java.util.List;
 public class AddPersonalTime extends AppCompatActivity implements RangeTimePickerDialog.ISelectedTime {
 
     int i = AlertsSaver.startKey;
-    static  final String names[] = {"שבת", "שישי", "חמישי", "רביעי" ,"שלישי", "שני",  "ראשון"};
-    static CheckBox [] daysArr = new CheckBox[7];
+    static final String names[] = {"שבת", "שישי", "חמישי", "רביעי", "שלישי", "שני", "ראשון"};
+    static CheckBox[] daysArr = new CheckBox[7];
     Spinner dropdown;
     List<Integer> keyList;
 
@@ -36,38 +37,37 @@ public class AddPersonalTime extends AppCompatActivity implements RangeTimePicke
         myList.add("Please select an Item");
         List<String> deleted = AlertsSaver.returnDeltedPlaces(this);
         AlertsSaver alert;
-         while (true) {
-             try {
-                 //Creates alert array and key array
-                 if (deleted.size() == 0 || (deleted.size() != 0 && !deleted.contains(getString(i)))) {
-                     alert = new AlertsSaver(this, Integer.toString(i));
-                     myList.add(alert.getAlertTitle());
-                     keyList.add(i);
-                 }
-                 i++;
-             }
-             catch (Exception e) {
-                 break;
-              }
+        while (true) {
+            try {
+                //Creates alert array and key array
+                if (deleted.size() == 0 || (deleted.size() != 0 && !deleted.contains(getString(i)))) {
+                    alert = new AlertsSaver(this, Integer.toString(i));
+                    myList.add(alert.getAlertTitle());
+                    keyList.add(i);
+                }
+                i++;
+            } catch (Exception e) {
+                break;
+            }
         }
 
         //read input array
-        for (int j = 0; j < names.length ; j++) {
+        for (int j = 0; j < names.length; j++) {
             //create the UI check box
             final LinearLayout ll = findViewById(R.id.linearLayoutId);
-            CheckBox cb = new  CheckBox(getApplicationContext());
+            CheckBox cb = new CheckBox(getApplicationContext());
             cb.setText(names[j]);
             ll.addView(cb);
             daysArr[j] = cb;
         }
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
 
-        dropdown  = findViewById(R.id.spinner);
+        dropdown = findViewById(R.id.spinner);
         //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<Object> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, myList);
         //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
-        FloatingActionButton b1=(FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton b1 = (FloatingActionButton) findViewById(R.id.fab);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,14 +79,15 @@ public class AddPersonalTime extends AppCompatActivity implements RangeTimePicke
                 dialog.setColorBackgroundHeader(R.color.colorPrimary); // Set Color of Background header dialog
                 dialog.setColorTextButton(R.color.colorPrimaryDark); // Set Text color of button
                 FragmentManager fragmentManager = getFragmentManager();
-                dialog.show(fragmentManager, "");     }
+                dialog.show(fragmentManager, "");
+            }
         });
 
     }
 
     public void onClick1(View v) {
         //todo save the updated alert days
-        boolean [] days = new boolean[7];
+        boolean[] days = new boolean[7];
         for (int i = 0; i < days.length; i++)
             days[i] = daysArr[i].isChecked();
         int listCount = dropdown.getSelectedItemPosition();
@@ -94,29 +95,34 @@ public class AddPersonalTime extends AppCompatActivity implements RangeTimePicke
             AlertsSaver alert = new AlertsSaver(this, Integer.toString(keyList.get(listCount - 1)));
             alert.setDays(this, days);
             Toast.makeText(this, "YOUR DAYS HAVE BEEN SAVED", Toast.LENGTH_SHORT).show();
-        }
-        else
+        } else
             Toast.makeText(this, "PLEASE SELECT AN ITEM", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void onSelectedTime(int hourStart, int minuteStart, int hourEnd, int minuteEnd) {
-            //todo save the updated alert hours
-            int listCount = dropdown.getSelectedItemPosition();
-            if (listCount > 0) {
-                AlertsSaver alert = new AlertsSaver(this, Integer.toString(keyList.get(listCount - 1)));
-                String[] arr = new String[2];
-                arr[0] = Integer.toString(hourStart) + ":" +
-                        Integer.toString(minuteStart);
-                arr[1] = Integer.toString(hourEnd) + ":" +
-                        Integer.toString(minuteEnd);
-                alert.setHours(this, arr);
-                Toast.makeText(this, alert.toString(), Toast.LENGTH_SHORT).show();
-            }
-            else
-                Toast.makeText(this, "PLEASE SELECT AN ITEM", Toast.LENGTH_SHORT).show();
+        //todo save the updated alert hours
+        int listCount = dropdown.getSelectedItemPosition();
+        if (listCount > 0) {
+            AlertsSaver alert = new AlertsSaver(this, Integer.toString(keyList.get(listCount - 1)));
+            String[] arr = new String[2];
+            arr[0] = Integer.toString(hourStart) + ":" +
+                    Integer.toString(minuteStart);
+            arr[1] = Integer.toString(hourEnd) + ":" +
+                    Integer.toString(minuteEnd);
+            alert.setHours(this, arr);
+            Toast.makeText(this, alert.toString(), Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(this, "PLEASE SELECT AN ITEM", Toast.LENGTH_SHORT).show();
 
+    }
+
+    public void moveToHome(View v) {
+        //todo moves to HomePage class
+        Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
+        startActivity(intent);
     }
 
 }
