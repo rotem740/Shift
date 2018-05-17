3722367f3ada6c30aeb4de45dfb90852b8c24046
Ori Mandelawi <omandelawi@gmail.com>
2018-05-17 14:29:02 +0300

    ended the sign up page
    changed the xml and java files and added a new user page

---
+++ app/src/main/java/com/kalay/shift/shift/User.java
@@ -0,0 +1,19 @@
+package com.kalay.shift.shift;
+
+import android.provider.MediaStore;
+
+/**
+ * Created by orim on 5/17/18.
+ */
+
+public class User {
+
+    private String name;
+    private String gender;
+
+    public User(String name, String gender){
+
+        this.name = name;
+        this.gender = gender;
+    }
+}
