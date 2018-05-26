0eb67f6c51fa63a1a21cf9c233cca9f0347d9460
Rom Dolinger <romdolinger@gmail.com>
2018-05-26 23:28:57 +0300

    HomePage

--- app/src/main/java/com/kalay/shift/shift/HomePageActivity.java
+++ app/src/main/java/com/kalay/shift/shift/HomePageActivity.java
@@ -1,7 +1,9 @@
 package com.kalay.shift.shift;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by gal zohar on 4/6/2018.
@@ -12,6 +14,18 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+    public void changeTimes(View x){
+        Intent myIntent = new Intent(this, AddPersonalTime.class);
+        startActivity(myIntent);
+    }
+    public void  changeInterests(View z){
+        Intent myIntent = new Intent(this, prefrencesChangesActivity.class);
+        startActivity(myIntent);
+    }
+    public void addNotifications(View y){
+        Intent myIntent = new Intent(this, SetAlert.class);
+        startActivity(myIntent);
     }
 
 }
